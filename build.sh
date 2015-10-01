9a19898664a1a03fc6402bb0b050ee55f06503e5
Adam Piekarczyk <adam1piekarczyk@gmail.com>
2015-10-01 19:46:04 +0200

    Build script update.

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@ PANDOC_OPTS="-f markdown_github+yaml_metadata_block -s --toc --toc-depth=3"
 CONTENT="metadata.yaml ???_*.md"
 
 pandoc -o informator.epub $PANDOC_OPTS $CONTENT
-pandoc --latex-engine=xelatex -o informator.pdf $PANDOC_OPTS $CONTENT
+pandoc --latex-engine=xelatex --template=template.tex -o informator.pdf $PANDOC_OPTS $CONTENT
 pandoc -o informator.html $PANDOC_OPTS $CONTENT
 # Nie chcę mieć buildu oznaczanego jako fail przez warningi kindlegen.
 kindlegen informator.epub || true 
