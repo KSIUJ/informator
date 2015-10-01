7b8c6762489853b85694fd6efa606009e0ad3d87
Adam Piekarczyk <adam1piekarczyk@gmail.com>
2015-10-01 21:54:15 +0200

    HTML template.

--- build.sh
+++ build.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-PANDOC_OPTS="-f markdown_github+yaml_metadata_block -s --toc --toc-depth=3"
+PANDOC_OPTS="-s -f markdown_github+yaml_metadata_block --toc --toc-depth=3"
 CONTENT="metadata.yaml ???_*.md"
 
 pandoc -o informator.epub $PANDOC_OPTS $CONTENT
 pandoc --latex-engine=xelatex --template=template.tex -o informator.pdf $PANDOC_OPTS $CONTENT
-pandoc -o informator.html $PANDOC_OPTS $CONTENT
+pandoc --template=template.html -o informator.html $PANDOC_OPTS $CONTENT
 # Nie chcę mieć buildu oznaczanego jako fail przez warningi kindlegen.
 kindlegen informator.epub || true 
