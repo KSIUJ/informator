fc4e2d9c293a7ac8d3dfeac90af97f44ff52cc74
Adam Piekarczyk <adam1piekarczyk@gmail.com>
2016-09-19 01:01:47 +0200

    Deleted kindlegen command.

--- build.sh
+++ build.sh
@@ -5,5 +5,3 @@ CONTENT="metadata.yaml ???_*.md"
 pandoc -o informator.epub $PANDOC_OPTS $CONTENT
 pandoc --latex-engine=xelatex --template=template.tex -o informator.pdf $PANDOC_OPTS $CONTENT
 pandoc --template=template.html -o informator.html $PANDOC_OPTS $CONTENT
-# Nie chcę mieć buildu oznaczanego jako fail przez warningi kindlegen.
-kindlegen informator.epub || true 
