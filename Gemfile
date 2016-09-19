3cba149a9f9f38074b0bcbc68ec5f57496780cf6
Adam Piekarczyk <adam1piekarczyk@gmail.com>
2016-09-19 02:04:27 +0200

    Added Jekyll skeleton.

---
+++ Gemfile
@@ -0,0 +1,10 @@
+source "https://rubygems.org"
+
+gem "jekyll", "3.2.1"
+# default style
+gem "minima"
+
+# If you have any plugins, put them here!
+# group :jekyll_plugins do
+#   gem "jekyll-github-metadata", "~> 1.0"
+# end
--- build.sh
+++ build.sh
@@ -2,6 +2,6 @@
 PANDOC_OPTS="-s -f markdown_github+yaml_metadata_block --toc --toc-depth=3"
 CONTENT="metadata.yaml ???_*.md"
 
-pandoc -o informator.epub $PANDOC_OPTS $CONTENT
-pandoc --latex-engine=xelatex --template=template.tex -o informator.pdf $PANDOC_OPTS $CONTENT
+pandoc -o static/informator.epub $PANDOC_OPTS $CONTENT
+pandoc --latex-engine=xelatex --template=template.tex -o static/informator.pdf $PANDOC_OPTS $CONTENT
 pandoc --template=template.html -o informator.html $PANDOC_OPTS $CONTENT
