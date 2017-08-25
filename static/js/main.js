6eb370a7edf200f791fc566f4f0675dcb5ca088c
zdomcia <dominikazajac00@gmail.com>
2017-08-25 20:42:25 +0200

    base styling of card on index.html

--- static/js/main.js
+++ static/js/main.js
@@ -1,8 +1,1 @@
-$('.card').click(function() {
-    const id = $(this).attr('id');
-    window.location.href = './' + id + '.html';
-});
 
-$('.section-header').click(function() {
-    $(this).next().toggle();
-});
